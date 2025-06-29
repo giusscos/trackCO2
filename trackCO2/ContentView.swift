48ec2121b80bca455fd6b083eb6032c1b9e278af
giusscos <giu.cosenza08@gmail.com>
2025-06-29 11:55:30 +0200

    Initial Commit

---
+++ trackCO2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trackCO2
+//
+//  Created by Giuseppe Cosenza on 29/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trackCO2/trackCO2App.swift
@@ -0,0 +1,17 @@
+//
+//  trackCO2App.swift
+//  trackCO2
+//
+//  Created by Giuseppe Cosenza on 29/06/25.
+//
+
+import SwiftUI
+
+@main
+struct trackCO2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
