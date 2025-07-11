036aeea8af3c1f549c4819c0cba44198aeb586cc
giusscos <giu.cosenza08@gmail.com>
2025-07-11 21:56:42 +0200

    update: paywall update

--- trackCO2/View/PaywallView.swift
+++ trackCO2/View/PaywallView.swift
@@ -17,43 +17,47 @@ struct PaywallView: View {
     
     var body: some View {
         NavigationStack {
-            SubscriptionStoreView(groupID: storeKit.groupId) {
-                VStack {
-                    Image(colorScheme == .dark ? "paywall-dark" : "paywall-light")
-                        .resizable()
-                        .frame(minWidth: 100, maxWidth: 350, minHeight: 100, maxHeight: 350)
-                        .aspectRatio(1/1, contentMode: .fit)
-                    
-                    Text("Track your carbon footprint")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                    
-                    Text("Add your personal activities and see how much CO2 you're saving every day.")
-                        .multilineTextAlignment(.center)
-                        .font(.title3)
-                        .foregroundColor(.secondary)
-                    
-                    Button {
-                        showLifetimePlans = true
-                    } label: {
-                        HStack (spacing: 8) {
-                            Text("Save with lifetime plans")
-                            
-                            Image(systemName: "chevron.right.circle.fill")
-                        }
-                        .font(.headline)
+            VStack {
+                SubscriptionStoreView(groupID: storeKit.groupId) {
+                    VStack {
+                        Image(colorScheme == .dark ? "paywall-dark" : "paywall-light")
+                            .resizable()
+                            .frame(minWidth: 150, maxWidth: 350, minHeight: 150, maxHeight: 350)
+                            .aspectRatio(1/1, contentMode: .fit)
+                        
+                        Text("Track your carbon footprint")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        
+                        Text("Add your personal activities and see how much CO2 you're saving every day.")
+                            .multilineTextAlignment(.center)
+                            .font(.title3)
+                            .foregroundColor(.secondary)
+                        
+                        
                     }
-                    .buttonStyle(.plain)
-                    .padding(.vertical)
                 }
+                .subscriptionStoreControlStyle(.pagedProminentPicker, placement: .bottomBar)
+                .subscriptionStoreButtonLabel(.multiline)
+                .storeButton(.visible, for: .restorePurchases)
+                .subscriptionStorePolicyDestination(url: URL(string: "https://giusscos.it/privacy")!, for: .privacyPolicy)
+                .subscriptionStorePolicyDestination(url: URL(string: "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/")!, for: .termsOfService)
+                .tint(.primary)
+                .interactiveDismissDisabled()
+                
+                Button {
+                    showLifetimePlans = true
+                } label: {
+                    HStack (alignment: .center, spacing: 8) {
+                        Text("Save with lifetime plans")
+                        
+                        Image(systemName: "chevron.right.circle.fill")
+                    }
+                    .font(.headline)
+                }
+                .foregroundStyle(.purple)
+                .buttonStyle(.plain)
             }
-            .subscriptionStoreControlStyle(.automatic, placement: .automatic)
-            .subscriptionStoreButtonLabel(.automatic)
-            .storeButton(.visible, for: .restorePurchases)
-            .subscriptionStorePolicyDestination(url: URL(string: "https://giusscos.it/privacy")!, for: .privacyPolicy)
-            .subscriptionStorePolicyDestination(url: URL(string: "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/")!, for: .termsOfService)
-            .tint(.primary)
-            .interactiveDismissDisabled()
             .sheet(isPresented: $showLifetimePlans) {
                 PaywallLifetimeView()
                     .presentationDetents(.init([.medium]))
